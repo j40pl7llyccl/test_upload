af0c6430b5fec9c401243efc294350ff972ef52c
j40pl7llyccl <j40pl7lly@gmail.com>
2025-03-02 22:33:22 +0800

    Create FormConfTrainingDraw.cs

---
+++ uIP.MacroProvider.TrainingConvert/FormConfTrainingDraw.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uIP.MacroProvider.TrainingConvert
+{
+    public partial class FormConfTrainingDraw : Form
+    {
+        public FormConfTrainingDraw()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
